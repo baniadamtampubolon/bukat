18ff7462ccd7f93c7c1395b8019469e9e2a7bc72
baniadamtampubolon <baniadam.tampubolon@gmail.com>
2024-10-26 13:23:20 +0700

    update main

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ import streamlit as st
 
 st.session_state.pindah = True
 
-Homepage = st.Page("Halaman Utama/halaman_utama.py",
-    title=" Profil Kelompok",
-    default=True)
-
 
 Mahasiswa8 = st.Page(
     "Buku Kating/075_Aliya Ammara Ananta.py",
@@ -22,7 +18,6 @@ Mahasiswa8 = st.Page(
 if st.session_state.pindah:
     pg = st.navigation(
         {
-            "Halaman Utama": [Homepage],
             "Buku Kating": [Mahasiswa8],
             
         }
